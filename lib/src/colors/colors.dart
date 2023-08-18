fb0e02dd762f548d795c22afd2f0807bbef0cb51
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-08-18 00:00:35 -0500

    add search and icons

--- lib/src/colors/colors.dart
+++ lib/src/colors/colors.dart
@@ -5,3 +5,5 @@ final greyColors = Color.fromRGBO(184, 187, 198, 1.0);
 final pinkColors = Color.fromRGBO(255, 45, 85, 1.0);
 final bgGreyColors = Color.fromRGBO(247, 248, 250, 1.0);
 final inputColors =Color.fromRGBO(142, 142, 147, 1.2);
+
+final borderColors =Color.fromRGBO(234, 236, 239, 1.0);
--- lib/src/features/presentation/tabs/explore_tab/View/explore_tab.dart
+++ lib/src/features/presentation/tabs/explore_tab/View/explore_tab.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../colors/colors.dart';
+
 class ExporeTabs extends StatelessWidget {
   const ExporeTabs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Explore"),
-    );
+    return SafeArea(
+        child: CustomScrollView(
+      slivers: [SliverList(delegate: SliverChildListDelegate(
+        [
+          _topBar(context)
+        ]
+      ))],
+    ));
   }
+}
+
+Widget _topBar(BuildContext context){
+  return Row(
+    children: [
+      Container(
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.only(left: 16, top: 10),
+        decoration: BoxDecoration(
+          border: Border.all(color: borderColors),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        width: 310,
+        child: Row(
+          children: [
+            Icon(Icons.search, size: 20.0, color: inputColors),
+            Container(
+              margin: EdgeInsets.only(left: 5.0),
+              child: Text("Buscar", style: TextStyle(
+                color: inputColors,
+                fontSize: 17.0
+              ),),
+            )
+          ],
+        ),
+      ),
+      Container(
+        width: 45.0,
+        height: 45.0,
+        margin: EdgeInsets.only(left: 10.0, top: 10),
+        decoration: BoxDecoration(
+          color: borderColors,
+          borderRadius: BorderRadius.circular(30)
+        ),
+        child: IconButton(
+          icon: Icon(Icons.filter_list, size: 30.0, color: Colors.white,),
+          onPressed: (){},),
+      )
+        
+    ],
+   );
 }
